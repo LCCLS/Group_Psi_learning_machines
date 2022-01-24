2663f0fddd81c84bdf9d96263db23aa00d2d6d30
Robin Stöhr <robinbux@MacBook-Pro-von-Robin.local>
2022-01-24 15:35:55 +0100

    Opencv

---
+++ BlobDetection/opencvtest.py
@@ -0,0 +1,28 @@
+# Standard imports
+import cv2
+import numpy as np
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 10
+params.maxThreshold = 200
+
+# Read image
+im = cv2.imread("test_pictures_0.png", cv2.IMREAD_COLOR)
+print(im)
+# Set up the detector with default parameters.
+
+detector = cv2.SimpleBlobDetector_create(params)
+
+# Detect blobs. SEGFAULT
+keypoints = detector.detect(im)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
