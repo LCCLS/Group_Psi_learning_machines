e5b6d694b17f04a4ce33898cd7fd42df99415ea6
Robin Stöhr <robinbux@MacBook-Pro-von-Robin.local>
2022-01-24 14:21:43 +0100

    Working Task2

--- BlobDetection/send_commands.py
+++ BlobDetection/send_commands.py
@@ -8,59 +8,57 @@ import robobo
 import cv2
 import sys
 import signal
+import prey
 
 
 def terminate_program(signal_number, frame):
     print("Ctrl-C received, terminating program")
     sys.exit(1)
 
-
 def main():
     signal.signal(signal.SIGINT, terminate_program)
 
     # rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
-    rob = robobo.SimulationRobobo().connect(address='127.0.0.1', port=19998)
+    rob = robobo.SimulationRobobo().connect(address='127.0.0.1', port=19997)
 
     rob.play_simulation()
 
-    # # Following code moves the robot
+    # Following code moves the robot
     for i in range(10):
-        print("robobo is at {}".format(rob.position()))
-        rob.move(5, 5, 2000)
+            print("robobo is at {}".format(rob.position()))
+            rob.move(5, 5, 2000)
+   
+    print("robobo is at {}".format(rob.position()))
+    rob.sleep(1)
 
-    # print("robobo is at {}".format(rob.position()))
-    # #rob.sleep(3)
-    #
     # Following code moves the phone stand
-    #rob.set_phone_pan(343, 100)
-    #rob.set_phone_tilt(109, 100)
-    #time.sleep(1)
+    rob.set_phone_pan(343, 100)
+    rob.set_phone_tilt(109, 100)
+    time.sleep(1)
     rob.set_phone_pan(11, 100)
     rob.set_phone_tilt(26, 100)
-    time.sleep(10)
-    #
-    # # Following code makes the robot talk and be emotional
-    # rob.set_emotion('happy')
-    # rob.talk('Hi, my name is Robobo')
-    # rob.sleep(1)
-    # rob.set_emotion('sad')
 
-    print("Before image")
-    time.sleep(5)
+    # Following code makes the robot talk and be emotional
+    rob.set_emotion('happy')
+    rob.talk('Hi, my name is Robobo')
+    rob.sleep(1)
+    rob.set_emotion('sad')
+
     # Following code gets an image from the camera
     image = rob.get_image_front()
-    print(image)
     # IMPORTANT! `image` returned by the simulator is BGR, not RGB
-    cv2.imwrite("test_pictures.png", image)
-    print("After image")
+    cv2.imwrite("test_pictures.png",image)
 
     time.sleep(0.1)
 
-    print("After reading")
+    # IR reading
+    for i in range(1000000):
+        print("ROB Irs: {}".format(np.log(np.array(rob.read_irs()))/10))
+        time.sleep(0.1)
 
     # pause the simulation and read the collected food
     rob.pause_simulation()
-
+    
     # Stopping the simualtion resets the environment
     rob.stop_world()
 
